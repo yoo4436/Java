e6a18de4cc932ace754daf625d3a17d2be1c4eff
Dennyy124986 <choustar4436@gmail.com>
2026-02-06 09:50:23 +0800

    02060950

--- Spring01/src/main/java/com/example/spring1/apis/Brad05.java
+++ Spring01/src/main/java/com/example/spring1/apis/Brad05.java
@@ -1,6 +1,5 @@
 package com.example.spring1.apis;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class Brad05 {
 	
 	@RequestMapping("/calc") // /brad05/calc?x=10&y=3
-	public void calc(@RequestParam String x, @RequestParam String y) {
+	public String calc(
+		@RequestParam(required= false, defaultValue = "0") String x, 
+		@RequestParam(required= false, defaultValue = "0") String y) {
+		// System.out.printf("x = %s : y = %s\n",x ,y);
+		// int sum = Integer.parseInt(x) + Integer.parseInt(y);
+		// System.out.printf("sum = %d\n",sum);
 		System.out.printf("x = %s : y = %s\n",x ,y);
-		int sum = Integer.parseInt(x) + Integer.parseInt(y);
-		System.out.printf("sum = %d\n",sum);
+		try {
+			int r = Integer.parseInt(x) + Integer.parseInt(y);
+			return r +"";
+		} catch (NumberFormatException e) {
+			return "ERROR";
+		}
 	}
 }
