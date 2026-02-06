4d91c050b19b03c2ae432efeb9675486c1881ae3
Dennyy124986 <choustar4436@gmail.com>
2026-02-06 12:02:42 +0800

    Spring-sql

--- Spring01/src/main/java/com/example/spring1/apis/Brad08.java
+++ Spring01/src/main/java/com/example/spring1/apis/Brad08.java
@@ -1,5 +1,32 @@
 package com.example.spring1.apis;
 
-public class Brad08 {
+import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/brad08")
+public class Brad08 {
+    @Autowired
+    private  NamedParameterJdbcTemplate jdbc;
+    
+    @RequestMapping("/test1")
+    public void test() {
+        String sql = """
+                insert into cust
+                    (cname, tel, birth)
+                values
+                    (:cname, :tel, :birth)
+                """;
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("cname", "111");
+        map.put("tel", "01-0562");
+        map.put("birth", "1999-05-07");
+
+        int n = jdbc.update(sql, map);
+        System.out.println(n);
+    }
 }
