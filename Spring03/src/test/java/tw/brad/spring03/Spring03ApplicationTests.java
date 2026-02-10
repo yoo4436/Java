f82df2a3f67681494007b80aeefadcd75e5c0dcb
Dennyy124986 <choustar4436@gmail.com>
2026-02-10 09:20:30 +0800

    Spring03

---
+++ Spring03/src/test/java/tw/brad/spring03/Spring03ApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.brad.spring03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
