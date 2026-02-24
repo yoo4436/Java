d940086c8522d6691f5bb20788471bab3189abf0
Dennyy124986 <choustar4436@gmail.com>
2026-02-24 11:02:33 +0800

    Spring07

---
+++ Spring07/src/main/java/tw/brad/spring04/Spring07Application.java
@@ -0,0 +1,13 @@
+package tw.brad.spring04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring07Application.class, args);
+	}
+
+}
