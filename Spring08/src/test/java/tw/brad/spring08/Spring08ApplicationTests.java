0f58d7a5f8fbed1a3bb96a43cef2cc5dc4dd8d06
Dennyy124986 <choustar4436@gmail.com>
2026-02-24 15:18:03 +0800

    Spring08

---
+++ Spring08/src/test/java/tw/brad/spring08/Spring08ApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.brad.spring08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
