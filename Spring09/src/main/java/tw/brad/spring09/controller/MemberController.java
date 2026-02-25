966b828306ab1b8f1b5d533c24e9acc98ded6aa1
Dennyy124986 <choustar4436@gmail.com>
2026-02-25 15:25:39 +0800

    remark: MemberController-控制會員 admin頁上方加上security(sec)過濾權限 系統核心SecurityConfig

--- Spring09/src/main/java/tw/brad/spring09/controller/MemberController.java
+++ Spring09/src/main/java/tw/brad/spring09/controller/MemberController.java
@@ -1,14 +1,38 @@
 package tw.brad.spring09.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/")
 @Controller
 public class MemberController {
 
-    @RequestMapping("/test")
-    public String test1() {
-        return "test1";
+    @RequestMapping("/")
+    public String root() {
+        return "redirect:/main";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+    @GetMapping("/main")
+    public String main(Model model) {
+        model.addAttribute("title", "會員主頁");
+        return "main";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        model.addAttribute("title", "管理員主頁");
+        return "admin";
+    }
+
+    @GetMapping("/page403")
+    public String page403() {
+        return "page403";
     }
 }
